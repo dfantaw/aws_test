424cd2e15ebab4bc5e0c98ee35e5651111e95ee1
Dawit D <banjawd@mskcc.org>
2025-03-13 13:47:46 -0400

    update yml

--- aws/main.tf
+++ aws/main.tf
@@ -119,7 +119,7 @@ resource "aws_s3_bucket_policy" "dotnet_app_bucket_policy" {
         Sid       = "PublicReadGetObject"
         Effect    = "Allow"
         Principal = "*"
-        Action    = ["s3:GetObject", "s3:PutObjectAcl","s3:PutObject","s3:GetObjectAcl","s3:DeleteObject"]
+        Action    = ["s3:GetObject","s3:PutObject"]
         Resource  = "${aws_s3_bucket.app_bucket.arn}/*"
       }
     ]
